9a6cd16a6a41a6d30935d334343e8fdd18701eb0
Dennis Nilsson <dennis@oapps.se>
2014-07-02 15:34:51 +0200

    ...

--- setup.sh
+++ setup.sh
@@ -4,21 +4,6 @@
 
 loadkeys sv-latin1
 
-#Connect to wireless network
-
-wifi-menu mlan0
-
-#Test internet connection
-
-wget -q --tries=10 --timeout=20 -O - http://google.com > /dev/null
-if [[ $? -eq 0 ]]; then
-	echo "Internet connection confirmed"
-	read -p "Press [enter] to continue with installation." KEY
-else
-	echo "No internet connection, will try to reconnect in 5 seconds"
-	sleep 5s; sh setup.sh
-fi
-
 #Install Arch Linux to eMMC
 
-	sh install.sh /dev/mmcblk0
+sh /root/scripts/install.sh /dev/mmcblk0
