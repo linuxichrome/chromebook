b4c7dee2e93250c6279fd17ad90e2c44a6b7ae35
Dennis Nilsson <dennis@oapps.se>
2014-07-04 15:43:39 +0200

    ...

--- chroot-install.sh
+++ chroot-install.sh
@@ -10,6 +10,7 @@ log() {
 USERNAME="studerande"
 
 CONFIG="/tmp/config"
+XORGCONF="/etc/X11/xorg.conf.d"
 
 #[Packages]
 #base="xorg-server xorg-xinit xorg-server-utils xf86-video-fbdev xf86-input-synaptics"
@@ -19,25 +20,31 @@ CONFIG="/tmp/config"
 log "Installerar program och konfigurationsfiler (Ta en kaffe under tiden...)"
 
 	pacman -U /tmp/installpkg/* --noconfirm --needed
-	ln -s /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
-	cp $CONFIG/50-touchpad.conf /etc/X11/xorg.conf.d
-	cp $CONFIG/00-keyboard.conf /etc/X11/xorg.conf.d
+	cp $CONFIG/50-touchpad.conf $XORGCONF
+	cp $CONFIG/00-keyboard.conf $XORGCONF
+	cp $CONFIG/10-evdev.conf $XORGCONF
 	cp $CONFIG/handler.sh /etc/acpi
 	#Wifi settings
 	cp $CONFIG/manager-settings.conf /etc/wicd
 	cp $CONFIG/wireless-settings.conf /etc/wicd
-
+	#Set usb mount point
 	cat $CONFIG/usbconf >> /etc/fstab
 	mkdir /mnt/usbstick
+	
+	#Enable services
 	systemctl enable kdm
 	systemctl enable wicd
 	systemctl enable acpid
+
+	#Set locale
+	localectl set-locale LANG="sv_SE.UTF-8"
+	ln -s /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
 
 log "Ange lösenord för $USERNAME:"
 
 	useradd -m -g users -G wheel,storage,power -s /bin/bash $USERNAME
 	passwd $USERNAME
-
+	echo '$USERNAME ALL=(ALL:ALL) ALL' >> /etc/sudoers
 #Extrahera och installera citrix
 
 log "Installerar Citrix."
